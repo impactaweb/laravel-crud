ba173ab6393d640dcfca7724cd4cba02564cec0f
jackson-impacta <jackson@impactaweb.com.br>
2020-02-13 11:04:30 -0300

    PSD-127 ModelBaseController e stub do controller customizado

---
+++ Controllers/ModelBaseController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Impactaweb\Crud\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ModelBaseController extends Controller
+{
+
+    protected $model;
+    public $validation;
+    public $relations;
+
+    /**
+     * Faz a seleção para qual URL o formulário direcionará
+     * o usuário, com base no botão clicado para submeter
+     * @paramm $idNovo ID do nova Model
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function redirecionar($idNovo = null)
+    {
+        return response()->json(['url' => FormUrls::redir('auto', $idNovo)]);
+    }
+
+    /**
+     * Salva a model de acordo com dados da Request
+     * @param array $dadosRequest
+     * @param array $relations
+     * @return mixed
+     */
+    protected function salvar(array $dadosRequest, array $relations = [])
+    {
+        $model = new $this->model();
+        $modelInstance = $model->saveFromRequest($dadosRequest, $relations);
+        return $modelInstance;
+
+    }
+
+    /**
+     * Salva e redireciona sem sequencia
+     * @param array $dadosRequest
+     * @param array $relations
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+    protected function salvarRedirecionar(array $dadosRequest, array $relations = [])
+    {
+        try {
+            $modelId = $this->salvar($dadosRequest, $relations)->getKey();
+            return $this->redirecionar($modelId);
+        } catch (\Exception $e){
+            return response()->json(['errors' => $e->getMessage()]);
+        }
+
+    }
+
+    public function store(Request $request)
+    {
+        if ($this->validation) {
+            $validation = new $this->validation();
+            $request->validate($validation->rules());
+        }
+
+        return $this->salvarRedirecionar($request->all(), $this->relations);
+    }
+
+    public function update(Request $request)
+    {
+        if ($this->validation) {
+            $validation = new $this->validation();
+            $request->validate($validation->rules());
+        }
+
+        return $this->salvarRedirecionar($request->all(), $this->relations);
+    }
+
+    /**
+     * Método desativado
+     */
+    public function show()
+    {
+        abort(404);
+    }
+
+
+}
