67b8f0f92333b26a94eeccd1672c007295041f3a
jackson-impacta <jackson@impactaweb.com.br>
2020-02-14 15:57:11 -0300

    Adicionado pasta components

---
+++ Form/Resources/assets/components/loadEditor.js
@@ -0,0 +1,52 @@
+(function($, axios){
+    $(document).ready(function(){
+        $summernote = $('[data-type="summernote"]')
+
+        if(!$summernote[0]) return
+
+        $summernote.summernote({
+            lang: 'pt-BR',
+            toolbar: [
+                // [groupName, [list of button]]
+                ['style', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['strikethrough']],
+                ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['height', ['height']],
+                ['insert', ['picture', 'table', 'link', 'video']],
+                ['misc', ['fullscreen', 'help']]
+            ],
+            codeviewFilter: false,
+            codeviewIframeFilter: true,
+            // Enviando em base64, caso queira sar a forma do v1 descomente esta a parte e adcione a rotta de upload
+            // callbacks: {
+            //     onImageUpload: function(files) {
+            //         const form = new FormData()
+            //         const config = {
+            //             headers: {'content-type': 'multipart/form-data'}
+            //         }
+
+            //         Array.from(files).forEach(function(file, idx){
+            //             form.append('media' + idx, file, file.name)
+            //         })
+
+            //         axios.post(END_POINT, form, config)
+            //         .then(function(res) {
+
+            //             console.log(res)
+            //             return
+            //             const imgUrl = res.data.imgUrl
+            //             const img = document.createElement('img')
+            //             img.src = imgUrl
+            //             $summernote.summernote('insertNode', img)
+            //         })
+            //         .catch(function(err){
+            //             alert('falha no upload da imagem, verifique a extensão da imagem')
+            //         })
+            //     }
+            // }
+        })
+    })
+})(jQuery, axios)
+
---
+++ Form/Resources/assets/components/loadingData.js
@@ -0,0 +1,33 @@
+(function($){
+    if($('[data-target="content"]').length){
+        const $loading = `
+        <div class="loading-container">
+            <div class="lds-roller">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        `
+        const err = 'kjdfjkdngdf'
+
+        function getList() {
+            const $paths = window.location.pathname.split('/')
+            //TODO: Fazer funcionar com paginação
+            const url = `${$paths.join('/')}/listagem`
+
+            $.get(url, function (list) {
+                $('[data-target="content"]').html(list);
+            })
+            // TODO: Fazer gargalo pra erro
+            // .catch(function(err) {
+
+            // })
+        }
+        $(document).ready(function () {
+            $('[data-target="content"]').html($loading);
+            getList()
+        })
+    }
+})(jQuery)
