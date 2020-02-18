94fdeab92c350ab77446612c2202917e4f1ff2fd
antunesgabriel <gabrielantunescontato@gmail.com>
2020-02-18 20:16:23 +0000

    javascript para upoad asyncrono

--- Form/Resources/assets/form.js
+++ Form/Resources/assets/form.js
@@ -1872,6 +1872,108 @@
                 maxHeight: 400,
             })
         })
+
+        function asyncFileUpload() {
+            const $inputs = $('[data-file="async-upload"]');
+            let inProgrees = false
+            const method = $('input[name="_method"').val()
+            const action = $('#form').attr('action')
+            if(!$inputs.length) return
+
+            function handleSuccess(res) {
+                const { error, success } = res.data;
+                if (error) {
+                    const items = Object.values(error).reduce(function(prev, cur) {
+                        return prev + ', ' + cur;
+                    }, '');
+                    alert('Falha no envio dos seguintes arquivos: ' + items);
+                    this.value = null;
+                    this.files.length = 0;
+                    return;
+                }
+
+                Object.keys(success).forEach(function(key) {
+                    const $info = $(`[mock-name="${key}"]`).parent('.form-group').find('[actions-container]');
+                    $info.find('[link-container]').attr('href', success[key].url).css('display', 'none');
+                    $info.css('display', 'block');
+                    $info.find('[destroy-file]').attr('destroy-file', success[key].hashName);
+                    $(`input[name="${key}"]`).val(success[key].hashName)
+                })
+
+                $info.css('display', 'block');
+                inProgrees = false;
+            }
+
+            function handleFailure() {
+                const $progressContainer = $(this).next('.progress.mt-1');
+                $progressContainer.find('.progress-bar').css('width', '0%');
+                $progressContainer.css('display', 'none');
+                inProgrees = false;
+            }
+
+            /**
+             * Handle input file
+             * @param {event} e
+             */
+            function handleChange(e) {
+                const files = new FormData();
+                files.append(this.getAttribute('mock-name'), this.files[0]);
+                const $progressContainer = $(this).next('.progress.mt-1');
+                $progressContainer.css('display', 'block');
+                const $progress = $progressContainer.find('.progress-bar');
+                $progress.css('width', '0%');
+                inProgrees = true;
+                axios[method.toLowerCase()](action, files, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    },
+                    onUploadProgress: function( progressEvent ) {
+                        let percentCompleted = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
+                        $progress.css('width', percentCompleted + '%').html(percentCompleted === 100 ? 'Concluído' :  (percentCompleted + '%'));
+                    },
+                }).then(handleSuccess)
+                .catch(handleFailure)
+            }
+
+
+            $inputs.each(function(idx, $input) {
+                let accept = $input.getAttribute('accept');
+                let extensions = null;
+                if (accept) {
+                    accept =  accept.split(',');
+                    extensions = accept.map(function(ext) {
+                        return ext.replace(/(\w*\/?\.?)(\w+[-?\w\.]*)/mi, '$2');
+                    });
+
+                    $input.setAttribute('data-ext', extensions.join(','));
+                }
+
+                $input.onchange = handleChange;
+                $($input).parent('.form-group')
+                    .find('[destroy-file]')
+                    .click(
+                        function (e) {
+                            e.preventDefault();
+                            const hash = $(this).attr('destroy-file');
+                            const $inputFile = $($input);
+                            const field = $inputFile.attr('mock-name');
+                            if (!hash) return
+                            axios.delete(`${action}?field=${field}&hash=${hash}`)
+                                .then(function(res) {
+                                    $inputFile.parent('.form-group').find('[actions-container]').css('display', 'none')
+                                    $inputFile.parent('.form-group').find('input[type="hidden"]').val(null);
+                                    $inputFile.parent('.form-group').find('.progress').css('display', 'none');
+                                    $inputFile.val(null)
+                                })
+                                .catch(function() {
+                                    alert('Falha ao excluir');
+                                });
+                        }
+                    );
+            });
+        }
+
+        asyncFileUpload()
     })($)
 })(jQuery)
 
