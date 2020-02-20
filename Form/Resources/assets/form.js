d57ee09c52aabcb569614bdcc54492535a956bb0
jackson-impacta <jackson@impactaweb.com.br>
2020-02-20 10:38:52 -0300

    Adicionado verificação de erros no form.js

--- Form/Resources/assets/form.js
+++ Form/Resources/assets/form.js
@@ -1823,6 +1823,22 @@
 
         function handleFailure(error) {
 
+            if (error.status >= 500) {
+                const alertError = `
+                <div class="container alert mb-1 alert-danger alert-dismissible fade show" role="alert" data-expect >
+                <span data-content>${error.responseJSON.errors}</span>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times</span>
+                </button>
+                </div>
+                `
+
+                $alert.innerHTML = alertError
+                $alert.scrollIntoView()
+                $('[data-container="loading"]').html('')
+                return
+            }
+
             if (error.status !== 422 || !error.responseJSON.errors) return
 
 
