481baae846a30b61032cd3d47b6d212155c691b2
Gabriel Antunes <45521465+antunesgabriel@users.noreply.github.com>
2020-02-21 09:34:53 -0300

    Create hidden.blade.php

---
+++ Form/Resources/views/fields/hidden.blade.php
@@ -0,0 +1,20 @@
+
+<div class="d-none">
+    <div>
+        <input
+          type="hidden"
+          class="form-control {{ $class }}"
+          id="c-{{$id}}"
+          name="{{$id}}"
+          value="{{ $value }}"
+
+          {{-- Atributos adicionais --}}
+          @foreach ($attrs as $attr => $attrValue)
+            @if(gettype($attrValue) == 'string')
+                {{ $attr }}="{{ $attrValue }}"
+            @endif
+          @endforeach
+          {{-- Fim - Atributos adicionais --}}
+        >
+    </div>
+</div>
