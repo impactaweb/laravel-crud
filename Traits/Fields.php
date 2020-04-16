914591a321a8b9e833e0e87a6d68af62065f0e85
jackson-impacta <57142037+jackson-impacta@users.noreply.github.com>
2020-04-16 16:27:57 -0300

    Create Fields.php

---
+++ Traits/Fields.php
@@ -0,0 +1,182 @@
+<?php
+
+
+namespace Impactaweb\Crud\Traits;
+
+
+trait Fields
+{
+    /**
+     * Campo de texto
+     * @param string $name
+     * @param string $label
+     * @param array $options
+     * @return mixed
+     */
+    public function text(string $name, string $label, array $options = [])
+    {
+        return $this->field('text', $name, $label, $options);
+    }
+    
+    public function file(string $name, string $label, $diretorio)
+    {
+        return $this->field('file', $name, $label, ['dir' => $diretorio]);
+    }
+    
+    /**
+     * Campo de números
+     * @param string $name
+     * @param string $label
+     * @param array $options
+     * @return mixed
+     */
+    public function password(string $name, string $label, array $options = [])
+    {
+        return $this->field('password', $name, $label, $options);
+    }
+
+    /**
+     * @param string $name
+     * @param string $label
+     * @param array $options
+     * @return mixed
+     */
+    public function number(string $name, string $label, array $options = [])
+    {
+        return $this->field('number', $name, $label, $options);
+    }
+
+    /**
+     * Campo com select
+     * @param string $name
+     * @param string $label
+     * @param array $options
+     * @return mixed
+     */
+    public function select(string $name, string $label, array $options = [])
+    {
+        return $this->field('select', $name, $label, ['options' => $options]);
+    }
+
+    /**
+     * Campo Text Area
+     * @param string $name
+     * @param string $label
+     * @param array $options
+     * @return mixed
+     */
+    public function textArea(string $name, string $label, array $options = [])
+    {
+        return $this->field('textarea', $name, $label, ['options' => $options]);
+    }
+
+    /**
+     * Campo de multiSelect
+     * @param string $name
+     * @param string $label
+     * @param array $options
+     * @return mixed
+     */
+    public function multiSelect(string $name, string $label, array $options = [])
+    {
+        return $this->field('multiselect', $name, $label, ['options' => $options]);
+    }
+
+    /**
+     * Campo flag, "Sim" ou "Não"
+     * @param string $name
+     * @param string $label
+     * @param array $options
+     * @return mixed
+     */
+    public function flag(string $name, string $label, array $options = [])
+    {
+        return $this->field('flag', $name, $label, $options);
+    }
+
+    /**
+     * Campo com conteúdo HTML livre
+     * @param $conteudo
+     * @return mixed
+     */
+    public function html($conteudo)
+    {
+        return $this->field('html', '', '', ['content'=> $conteudo]);
+    }
+
+    /**
+     * Campo para mostrar IDS e links
+     * Caso o conteúdo for vazio, ele esconde o campo
+     * @param $label
+     * @param $conteudo
+     * @return mixed
+     */
+    public function show($label, $conteudo, $hideEmpty = true)
+    {
+        if ($hideEmpty && empty($conteudo)) {
+            return $this;
+        }
+        return $this->field('show', '', $label, ['content'=> $conteudo]);
+    }
+
+    /**
+     * Campo oculto
+     * @param $name
+     * @param $label
+     * @param $conteudo
+     * @return mixed
+     */
+    public function hidden($name, $label, $conteudo = '')
+    {
+        return $this->field('hidden', $name, $label, ['content'=> $conteudo]);
+    }
+
+    /**
+     * Campo de data
+     * @param string $name
+     * @param string $label
+     * @param array $options
+     * @return mixed
+     */
+    public function dateTime(string $name, string $label, array $options = [])
+    {
+        return $this->field('datetime', $name, $label, $options);
+    }
+
+    /**
+     * Campo de hora
+     * @param string $name
+     * @param string $label
+     * @param array $options
+     * @return mixed
+     */
+    public function time(string $name, string $label, array $options = [])
+    {
+        return $this->field('time', $name, $label, $options);
+    }
+
+    /**
+     * Editor de Rich Text
+     * @param string $name
+     * @param array $options
+     * @return mixed
+     */
+    public function rtf(string $name, array $options = [])
+    {
+        return $this->field('rtf', $name, '', $options);
+    }
+
+    /**
+     * MultiSelect com grupos
+     * @param string $name
+     * @param string $label
+     * @param array $options
+     * @return mixed
+     */
+    public function multiSelectGroup(string $name, string $label, array $options = [])
+    {
+        return $this->field('multiselectgroup', $name, $label, ['options' => $options]);
+    }
+
+
+}
