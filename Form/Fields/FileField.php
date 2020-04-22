5dcea7b3410e5a155932bbdb650232c678c8bb58
jackson-impacta <jackson@impactaweb.com.br>
2020-04-22 16:30:52 -0300

    Removido formatação automática de diretório

--- Form/Fields/FileField.php
+++ Form/Fields/FileField.php
@@ -6,12 +6,4 @@ namespace Impactaweb\Crud\Form\Fields;
 class FileField extends BaseField
 {
     protected $dir = '';
-
-    public function render(array $initial = [], array $rules = [])
-    {
-        if (!empty($this->dir)) {
-            $this->dir = '/' . ltrim($this->dir, '/');
-        }
-        return parent::render($initial, $rules);
-    }
 }
