3bf031de68a7802e0c21a4f5ca91528f25b25013
Antunes Gabriel <gabrielantunescontato@gmail.com>
2020-07-08 16:44:51 -0300

    fix: nested fields

--- src/js/components/handleFailureSendForm.js
+++ src/js/components/handleFailureSendForm.js
@@ -19,17 +19,42 @@ module.exports = function handleFailureSendForm(error) {
 
   let hasScroll = false;
 
+  `
+  {
+    "message": "The given data was invalid.",
+    "errors": {
+      "tipoisencao.teste.resultado": [
+          "O campo tipoisencao.teste.resultado deve ser um endereco"
+        ]
+      }
+    }
+  `
+
   // Erros enviados pela request do laravel
   Object.keys(camposInvalidos).forEach(function(name) {
-    let input;
-    const erros = "<li>" + camposInvalidos[name].join("</li><li>") + "</li>";
+    let inputName;
+
     if (name.includes(".")) {
-      name = name.split(".");
-      input = '[name="' + name[0] + "[" + name[1] + ']"]';
+      const paths = name.replace('..', '.').split(".");
+      inputName = `[name="${paths.reduce((prev, cur) =>  `${prev}[${cur}]`)}"]`; // js love reduce + arrow function
     } else {
-      input = '[name="' + name + '"]';
+      inputName = '[name="' + name + '"]';
     }
-    const $input = window.jQuery(input);
+
+    const $input = window.jQuery(inputName);
+    const $label = $input.parent().prev();
+    let erros = null;
+    if ($label.is('label')) {
+      erros = camposInvalidos[name].reduce( (prev, cur) => `${prev}<li>${cur
+        .replace(
+            name,
+            $label.html().replace(/(\n\s+ | \s+\n+)+/gim, // regex <3
+              ''
+          )
+        )}</li>`,'');
+    } else {
+      erros = camposInvalidos[name].reduce( (prev, cur) => `${prev}<li>${cur}</li>`,'');
+    }
 
     $input.hasClass("is-invalid") ? null : $input.toggleClass("is-invalid");
     $input.next(".invalid-feedback").html(erros);
