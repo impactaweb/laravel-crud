d6bbfa3a2b0a90fe0cf7e54ee9eb69fb8c58a6ad
jacksonpm <jacksonpmoura@hotmail.com>
2020-07-21 09:18:23 -0300

    PSD-397 Modificado template do hidden

--- Form/Resources/views/fields/hidden.blade.php
+++ Form/Resources/views/fields/hidden.blade.php
@@ -1,22 +1,16 @@
-<div class="form-group row align-items-center">
 
-    <div class="@if($col >= '10') col @else col-md-{{$col}} @endif">
-        <input
-            type="hidden"
-            class="form-control {{ $class }}"
-            id="c-{{$id}}"
-            name="{{$id}}"
-            value="{{ $value }}"
+<input
+        type="hidden"
+        class="form-control {{ $class }}"
+        id="c-{{$id}}"
+        name="{{$id}}"
+        value="{{ $value }}"
 
-        {{-- Atributos adicionais --}}
-        @foreach ($attrs as $attr => $attrValue)
-            @if(gettype($attrValue) == 'string')
-                {{ $attr }}="{{ $attrValue }}"
-            @endif
-        @endforeach
-        {{-- Fim - Atributos adicionais --}}
-
-        >
-        <div class="invalid-feedback"></div>
-    </div>
-</div>
+    {{-- Atributos adicionais --}}
+    @foreach ($attrs as $attr => $attrValue)
+        @if(gettype($attrValue) == 'string')
+            {{ $attr }}="{{ $attrValue }}"
+        @endif
+    @endforeach
+    {{-- Fim - Atributos adicionais --}}
+>
