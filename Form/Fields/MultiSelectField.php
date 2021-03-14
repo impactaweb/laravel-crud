ee76f19073df6c651c4055998f6c9916528d6c44
Gustavo Sagrillo <gustavosagr@gmail.com>
2021-03-14 13:07:11 -0300

    Removendo codigo desnecessario

--- Form/Fields/MultiSelectField.php
+++ Form/Fields/MultiSelectField.php
@@ -6,12 +6,4 @@ class MultiSelectField extends BaseField
 {
     protected $filter = false;
     protected $value = [];
-
-    protected function buildInitialValue(array $initial): void
-    {
-        parent::buildInitialValue($initial);
-        if (!is_array($this->value)) {
-            $this->value = [];
-        }
-    }
 }
--- Form/Fields/MultiSelectGroupField.php
+++ Form/Fields/MultiSelectGroupField.php
@@ -7,12 +7,4 @@ class MultiSelectGroupField extends BaseField
     protected $filter = false;
     protected $value = [];
     protected $selectOptions;
-
-    protected function buildInitialValue(array $initial): void
-    {
-        parent::buildInitialValue($initial);
-        if (!is_array($this->value)) {
-            $this->value = [];
-        }
-    }
 }
