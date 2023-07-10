877a05ff083dc07af05bf56c9d2a6f85ea8487a5
Lucas Izeffler <lucas123izeffler@gmail.com>
2023-07-10 11:25:38 -0300

    Inclusas novas mascaras

--- src/js/customs/mask.js
+++ src/js/customs/mask.js
@@ -1,77 +1,96 @@
 (function ($) {
-    $(document).ready(function () {
-          const hasInputWithMask = $("[data-input]")[0];
+  $(document).ready(function () {
+    const hasInputWithMask = $("[data-input]")[0];
 
-          if (!hasInputWithMask) {
-          return false;
-          }
+    if (!hasInputWithMask) {
+      return false;
+    }
 
-          Inputmask({
-              mask: '99/99/9999',
-              placeholder: '__/__/____'
-          }).mask(document.querySelectorAll('[data-input="date"]'));
+    Inputmask({
+      mask: '99/99/9999',
+      placeholder: '__/__/____'
+    }).mask(document.querySelectorAll('[data-input="date"]'));
 
-          Inputmask({
-              mask: '99/99/9999 99:99:99',
-              placeholder: '__/__/____ __:__:__'
-          }).mask(document.querySelectorAll('[data-input="date-time"]'))
+    Inputmask({
+      mask: '99/99/9999 99:99:99',
+      placeholder: '__/__/____ __:__:__'
+    }).mask(document.querySelectorAll('[data-input="date-time"]'))
 
-          Inputmask({
-              mask: '99:99:99',
-              placeholder: '__:__:__'
-          }).mask(document.querySelectorAll('[data-input="time"]'));
+    Inputmask({
+      mask: '99:99:99',
+      placeholder: '__:__:__'
+    }).mask(document.querySelectorAll('[data-input="time"]'));
 
-          Inputmask({
-              mask: '999.999.999-99',
-              placeholder: '___.___.___-__'
-          }).mask(document.querySelectorAll('[data-input="cpf"]'));
+    Inputmask({
+      mask: '999.999.999-99',
+      placeholder: '___.___.___-__'
+    }).mask(document.querySelectorAll('[data-input="cpf"]'));
 
-          Inputmask({
-              mask: '99999-999',
-              placeholder: '_____-___',
-          }).mask(document.querySelectorAll('[data-input="cep"]'));
+    Inputmask({
+      mask: '99999-999',
+      placeholder: '_____-___',
+    }).mask(document.querySelectorAll('[data-input="cep"]'));
 
-          Inputmask({
-              alias: 'numeric',
-              groupSeparator: ',',
-              digits: 2,
-              digitsOptional: false,
-              prefix: 'R$',
-              placeholder: '0'
-          }).mask(document.querySelectorAll('[data-input="money"]'));
+    Inputmask({
+      alias: 'numeric',
+      groupSeparator: ',',
+      digits: 2,
+      digitsOptional: false,
+      prefix: 'R$',
+      placeholder: '0'
+    }).mask(document.querySelectorAll('[data-input="money"]'));
 
-          Inputmask({
-              mask: '99999-9999',
-              placeholder: '____-____',
-          }).mask(document.querySelectorAll('[data-input="cel"]'));
+    Inputmask({
+      mask: '99999-9999',
+      placeholder: '____-____',
+    }).mask(document.querySelectorAll('[data-input="cel"]'));
 
-          Inputmask({
-              alias: 'numeric',
-              placeholder: '0',
-          }).mask(document.querySelectorAll('[data-input="number"]'));
+    Inputmask({
+      alias: 'numeric',
+      placeholder: '0',
+    }).mask(document.querySelectorAll('[data-input="number"]'));
 
-          let maskCelDDD = function(event) {
-            if($(this).val().replace(/\D/g, "").length == 11){
-                $(this).inputmask('(99)99999-999[9]');
-            } else {
-                $(this).inputmask('(99)9999-9999[9]');
-            }
-          };
-          $('[data-input="cel_with_ddd"]').on('keyup', maskCelDDD);
-          $('[data-input="cel_with_ddd"]').on('blur', maskCelDDD).trigger('blur');
+    let maskCelDDD = function (event) {
+      if ($(this).val().replace(/\D/g, "").length == 11) {
+        $(this).inputmask('(99)99999-999[9]');
+      } else {
+        $(this).inputmask('(99)9999-9999[9]');
+      }
+    };
+    $('[data-input="cel_with_ddd"]').on('keyup', maskCelDDD);
+    $('[data-input="cel_with_ddd"]').on('blur', maskCelDDD).trigger('blur');
 
-          Inputmask({
-              mask: '9999-9999',
-              placeholder: '____-____'
-          }).mask(document.querySelectorAll('[data-input="phone"]'))
+    Inputmask({
+      mask: '9999-9999',
+      placeholder: '____-____'
+    }).mask(document.querySelectorAll('[data-input="phone"]'))
 
-          Inputmask({
-              mask: '(99) 9999-9999',
-              placeholder: '(__) ____-____'
-          }).mask(document.querySelectorAll('[data-input="phone_with_ddd"]'))
+    Inputmask({
+      mask: '(99) 9999-9999',
+      placeholder: '(__) ____-____'
+    }).mask(document.querySelectorAll('[data-input="phone_with_ddd"]'))
 
-          Inputmask({
-              alias: 'email'
-          }).mask(document.querySelectorAll('[data-input="email"]'));
-    });
-  })(window.jQuery);
+    Inputmask({
+      alias: 'email'
+    }).mask(document.querySelectorAll('[data-input="email"]'));
+
+    Inputmask({
+      mask: '99/9999',
+      placeholder: '__/____'
+    }).mask(document.querySelectorAll('[data-input="mY"]'));
+
+    Inputmask({
+      mask: '99.999.999/9999-99',
+      placeholder: '__.___.___/____-__'
+    }).mask(document.querySelectorAll('[data-input="cnpj"]'));
+
+    Inputmask({
+      alias: 'numeric',
+      placeholder: '0',
+    }).mask(document.querySelectorAll('[data-input="only-number"]'));
+
+    $('[data-input="only-number"]').on('keyup', function () {
+      $(this).value = $(this).val().replace(/\D/g, "");
+    })
+  });
+})(window.jQuery);
